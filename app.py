2ff1b39d4e1605fc74770713713d0996be9a5111
ishita <ishitagops12@gmail.com>
2021-06-16 20:36:28 +0530

    updated

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+app  = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("final.html")
